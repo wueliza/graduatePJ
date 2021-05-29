c29ea18f6ee844f061518d1fccfd6b51e88e1d94
Eliza Wu <wueliza21@gmail.com>
2021-05-29 23:55:55 +0800

    ttt

--- app/src/main/java/com/project/graduatepj/Chemopm.java
+++ app/src/main/java/com/project/graduatepj/Chemopm.java
@@ -8,23 +8,45 @@ import android.view.View;
 import android.widget.Button;
 
 public class Chemopm extends AppCompatActivity {
-    private Button button;
+    private Button button1 , button2 , button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemopm);
 
-        button = (Button)findViewById(R.id.button1);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
 
-        Button nextPageBtn = (Button)findViewById(R.id.button1);
-
-        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent();
 
                 intent.setClass(Chemopm.this , Chemopm2.class);
+
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+
+                intent.setClass(Chemopm.this , Chemopm2.class);
+
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+
+                intent.setClass(Chemopm.this , Chemopm3.class);
 
                 startActivity(intent);
             }
