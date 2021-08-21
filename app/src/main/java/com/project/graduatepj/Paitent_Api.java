8b93de11aaa2c25b7a15ea5982dff45cfd2168ca
咩咩羊 <zsee6216@gmail.com>
2021-08-21 09:50:45 +0800

    singactivity測試api連接

--- app/src/main/java/com/project/graduatepj/Paitent_Api.java
+++ app/src/main/java/com/project/graduatepj/Paitent_Api.java
@@ -1,2 +1,58 @@
 package com.project.graduatepj;public class Paitent_Api {
+
+    private int patientNum;
+    private String sex;
+    private String birth;
+    private String bedNum;
+    private String bloodType;
+    private String name;
+    private String division;
+    private String height;
+    private String age;
+    private String weight;
+    private String bsno;
+
+    public int getPatientNum() {
+        return patientNum;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public String getBedNum() {
+        return bedNum;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getBsno() {
+        return bsno;
+    }
 }
--- app/src/main/java/com/project/graduatepj/SignActivity.java
+++ app/src/main/java/com/project/graduatepj/SignActivity.java
@@ -8,11 +8,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
@@ -22,9 +25,17 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class SignActivity extends AppCompatActivity {
     private Button bt;
     private Button bt2;
+    private TextView show;
+    private EditText input;
     SurfaceView surfaceView;
     TextView textView;
     CameraSource cameraSource;
@@ -35,11 +46,39 @@ public class SignActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
+        input = findViewById(R.id.input);
+        show = findViewById(R.id.show);
+
         //相機製作
         getPermissionsCamera();
 
         surfaceView=(SurfaceView)findViewById(R.id.surfaceView);
         textView=(TextView)findViewById(R.id.textView);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://106.105.167.136:8080/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(input.getText().toString() != null){
+                    Get_one(retrofit,editable.toString());
+                }
+                show.setText(editable);
+            }
+        });
+
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.ALL_FORMATS).build();
         cameraSource = new CameraSource.Builder(this,barcodeDetector).setAutoFocusEnabled(true).build();
@@ -157,4 +196,26 @@ public class SignActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},1);
         }
     }
+
+    public void Get_one(Retrofit retrofit,String id){
+        RESTfulApi jsonPlaceHolderApi = retrofit.create(RESTfulApi.class);
+        Call<Paitent_Api> call = jsonPlaceHolderApi.getOne(id);
+        call.enqueue(new Callback<Paitent_Api>() {
+            @Override
+            public void onResponse(Call<Paitent_Api> call, Response<Paitent_Api> response) {
+                if(!response.isSuccessful()){
+                    show.setText("找不到這個id");
+                    return;
+                }
+                String name = response.body().getName();
+                show.setText(name);
+            }
+
+            @Override
+            public void onFailure(Call<Paitent_Api> call, Throwable t) {
+                show.setText("請掃描條碼");
+            }
+        });
+    }
+
 }
