247ac0943aad7f072b71586856667c35697926ea
Eliza Wu <wueliza21@gmail.com>
2021-08-25 23:37:19 +0800

    衛材檢查API設定 & Patient API 拼字校正

--- app/src/main/java/com/project/graduatepj/RESTfulApi.java
+++ app/src/main/java/com/project/graduatepj/RESTfulApi.java
@@ -12,16 +12,16 @@ import retrofit2.http.Path;
 
 public interface RESTfulApi {
     @GET("patient")
-    Call<List<Paitent_Api>> getPosts();
+    Call<List<Patient_Api>> getPosts();
 
     @GET("patient/{patientNum}") //1 . 2 . 3 ...
-    Call<Paitent_Api> getOne(@Path("patientNum")String patientNum);
+    Call<Patient_Api> getOne(@Path("patientNum")String patientNum);
 
     @GET("staff/{emid}") //A00001 ...
     Call<Staff_Api> get_staff(@Path("emid")String emid);
 
     @POST("patient")
-    Call<Paitent_Api> postData(@Body Paitent_Api paitentApi);
+    Call<Patient_Api> postData(@Body Patient_Api paitentApi);
 
     @GET("eisai/{eisaiNum}")
     Call<Eisai_Api> get_eisai(@Path("eisaiNum")String eisaiNum);
--- app/src/main/java/com/project/graduatepj/eisairesult.java
+++ app/src/main/java/com/project/graduatepj/eisairesult.java
@@ -1,13 +1,22 @@
 package com.project.graduatepj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class eisairesult extends AppCompatActivity {
     private TextView staff_id , patient_id , eisai_id , result_tv;
-    private TextView resultTv2;
+    private Button completebt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +26,7 @@ public class eisairesult extends AppCompatActivity {
         patient_id = findViewById(R.id.patient_id);
         eisai_id = findViewById(R.id.eisai_id);
         result_tv = findViewById(R.id.result_tv);
+        completebt = findViewById(R.id.completebt);
 
         Bundle scan_result = this.getIntent().getExtras();
         String staff = scan_result.getString("staff_id");
@@ -27,6 +37,40 @@ public class eisairesult extends AppCompatActivity {
         patient_id.setText(patient);
         eisai_id.setText(eisai);
 
+        //api連接
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://106.105.167.136:8080/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        Get_eisai(retrofit , eisai);
+
+        completebt.setOnClickListener(this::complete);
+    }
+    public void Get_eisai(Retrofit retrofit, String id) {
+        RESTfulApi jsonPlaceHolderApi = retrofit.create(RESTfulApi.class);
+        Call<Eisai_Api> call = jsonPlaceHolderApi.get_eisai(id); //A00010
+        call.enqueue(new Callback<Eisai_Api>() {
+            @Override
+            public void onResponse(Call<Eisai_Api> call, Response<Eisai_Api> response) {
+                if (!response.isSuccessful()) {
+                    result_tv.setText("此id不存在，請重新掃描！");
+                    return;
+                }
+                else {
+                    String name = response.body().getName();
+                    result_tv.setText(name);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Eisai_Api> call, Throwable t) { }
+        });
+    }
+
+    private void complete(View v){
+        Intent intent = new Intent();
+        intent.setClass(eisairesult.this , gotofunction.class);
+        startActivity(intent);
     }
 }
