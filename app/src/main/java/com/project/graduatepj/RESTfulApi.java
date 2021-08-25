6c5c8153677380bb1b68d9a611d21b9d2096c0a0
Tracy <ww03975@gmail.com>
2021-08-25 15:00:35 +0800

    API

--- app/src/main/java/com/project/graduatepj/RESTfulApi.java
+++ app/src/main/java/com/project/graduatepj/RESTfulApi.java
@@ -17,14 +17,12 @@ import retrofit2.http.Path;
 public interface RESTfulApi {
     @GET("patient")
     Call<List<Paitent_Api>> getPosts();
-<<<<<<< HEAD
     @GET("patient/{patientNum}")
     Call<Paitent_Api> getOne(@Path("patientNum")String patientNum);
     @GET("staff/{emid}")
     Call<Staff_Api> get_staff(@Path("emid")String emid);
     Call<Paitent_Api> getOne(@Path("patientNum") String patientNum);
 ////2021/8/18/////
-=======
 
     @GET("patient/{patientNum}") //1 . 2 . 3 ...
 
@@ -33,7 +31,7 @@ public interface RESTfulApi {
     @GET("staff/{emid}") //A00001 ...
     Call<Staff_Api> get_staff(@Path("emid")String emid);
 
->>>>>>> 507e7ac434364fdd84eec8cd06800a0c89fd8765
     @POST("patient")
     Call<Paitent_Api> postData(@Body Paitent_Api paitentApi);
+
 }
