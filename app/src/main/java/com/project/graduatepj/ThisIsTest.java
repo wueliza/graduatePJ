d7aa7bf36df2c85eae93630331cfe44706c40ac2
wangihsin <tree8454@gmail.com>
2021-10-18 11:53:28 +0800

    新增一個layout 測試用

---
+++ app/src/main/java/com/project/graduatepj/ThisIsTest.java
@@ -0,0 +1,14 @@
+package com.project.graduatepj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThisIsTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_this_is_test);
+    }
+}
